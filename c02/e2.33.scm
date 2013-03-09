7e0c1f365d04e8a0d2549f2bc68d0a09953e7f11
Devon Olivier <medevon@gmail.com>
2013-03-09 17:43:44 -0400

    finish exercise 2.33

--- c02/e2.33.scm
+++ c02/e2.33.scm
@@ -1,7 +1,24 @@
 #lang planet neil/sicp
 
-(define (mmap p sequence)
-  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+(define (accumulatel operation initial sequence)
+  (cond ((null? sequence) initial)
+        (else (accumulateL operation (operation initial (car sequence))
+                          (cdr sequence)))))
 
-(define (square x) (* x x))
-(mmap square '(1 2 3))
+(define (accumulater operation initial sequence)
+  (cond ((null? sequence) initial)
+        (else (operation (car sequence)
+                         (accumulateR operation 
+                                      initial 
+                                      (cdr sequence))))))
+
+(define (mymap function sequence)
+  (accumulater (lambda (x y) (cons (function x) y)) nil sequence))
+
+(define (myappend seq1 seq2)
+  (accumulater cons seq2 seq1))
+
+(define (length sequence)
+  (accumulater (lambda (x y) (+ y 1))
+               0
+               sequence))
