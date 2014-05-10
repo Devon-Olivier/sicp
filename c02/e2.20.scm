0eb52399132a07ee486def551d4329f13b47d345
Devon Olivier <medevon@gmail.com>
2014-05-10 07:44:52 -0400

    remove old code

--- c02/e2.20.scm
+++ c02/e2.20.scm
@@ -1,19 +1,4 @@
-#lang planet neil/sicp
-
-(define (filter lst predicate)
-  (cond ((null? lst) lst)
-        ((predicate (car lst)) (cons (car lst) (filter (cdr lst) predicate)))
-        (else (filter (cdr lst) predicate))))
-
-#'(define (same-parity lst)
-  (if (null? lst)
-      lst
-      (filter lst (parity (car lst)))))
-
-(define (parity n)
-  (if (even? n)
-      even?
-      odd?))
+#lang racket
 
 (define (same-parity n . rest)
   (cons n (filter (lambda (m) (even? (+ n m))) rest)))
